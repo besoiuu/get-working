4685032de3681191b1dac71928432d18da870185
besoiuu <patrickardelean1@gmail.com>
2023-04-04 01:40:47 +0300

    added firebaseConfig in .env

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,14 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyC37Czp9FXxlEUcs4C3gctDhTnPEt8Cf1M",
-  authDomain: "get-working-edf01.firebaseapp.com",
-  projectId: "get-working-edf01",
-  storageBucket: "get-working-edf01.appspot.com",
-  messagingSenderId: "864434557897",
-  appId: "1:864434557897:web:ba1f3c950757e38cc70e18",
-  measurementId: "G-8RM79ZX461",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
