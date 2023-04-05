b47888bdb0237ac30affe50861914a1e50427984
besoiuu <patrickardelean1@gmail.com>
2023-04-05 22:03:52 +0300

    added icon for back button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import '../styles/Header.css'
+import React from "react";
+import "../styles/Header.css";
 import { useNavigate } from "react-router";
+import { FcUndo } from "react-icons/fc";
 
 export default function Header() {
   const navigate = useNavigate();
 
   const handleGoBack = () => {
-    navigate('/');
-  }
+    navigate("/");
+  };
 
   return (
     <header className="header">
-        <h1 className="title">Get Working</h1>
-        <button className='btn btn-secondary back' onClick={() => handleGoBack()}>Go Back</button>
-      </header>
-  )
+      <h1 className="title">Get Working</h1>
+      <button className="btn btn-secondary back" onClick={() => handleGoBack()}>
+        <FcUndo/>
+      </button>
+    </header>
+  );
 }
