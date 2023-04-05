2132982b8ba2802887e9e66720a7dedd1d66f2c8
besoiuu <patrickardelean1@gmail.com>
2023-04-05 18:10:19 +0300

    added go back function

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import '../styles/Header.css'
+import { useNavigate } from "react-router";
 
 export default function Header() {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate('/');
+  }
+
   return (
     <header className="header">
         <h1 className="title">Get Working</h1>
+        <button className='btn btn-secondary back' onClick={() => handleGoBack()}>Go Back</button>
       </header>
   )
 }
